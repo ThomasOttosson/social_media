328cb23739d523b83b45c5d2a8196356948f87ab
Thomas Ottosson <tomasottosson1@gmail.com>
2025-02-27 22:15:45 +0100

    added post model structure and migrated

--- env.py
+++ env.py
@@ -2,3 +2,4 @@ import os
 
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:0kR8njgyqobQ@ep-soft-sky-a21qtabt.eu-central-1.aws.neon.tech/water_hung_jam_822888")
+os.environ.setdefault("SECRET_KEY", "tomas")
