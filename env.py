dcf21c54f895e43cf1ddf3be9c90f9068f960413
Thomas Ottosson <tomasottosson1@gmail.com>
2025-03-09 22:55:40 +0100

    added cloudinary and deployment test

--- env.py
+++ env.py
@@ -3,3 +3,6 @@ import os
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:0kR8njgyqobQ@ep-soft-sky-a21qtabt.eu-central-1.aws.neon.tech/water_hung_jam_822888")
 os.environ.setdefault("SECRET_KEY", "tomas")
+os.environ.setdefault(
+    "CLOUDINARY_URL", "cloudinary://919518326595331:ilsF8b0ou4TAKa9gch3ILskwu3Q@ddtcx5nyz"
+)
