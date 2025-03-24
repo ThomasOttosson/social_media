65411b5b49b9e15f9c655f1e081cfdf6c10815a7
Thomas Ottosson <tomasottosson1@gmail.com>
2025-03-24 11:40:54 +0100

    added readme info

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # social_media
+
+# Credits
+* The majority of this code was inspired by Code Institute's student project called "codestar". Here is a link to Code Institute's website: https://codeinstitute.net/
--- blog/views.py
+++ blog/views.py
@@ -4,112 +4,107 @@ from django.contrib import messages
 from django.http import HttpResponseRedirect
 from .models import Post, Comment
 from .forms import CommentForm
-
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 
 class PostList(generic.ListView):
-   queryset = Post.objects.filter(status=1)
-   template_name = "blog/index.html"
-   paginate_by = 6
+    queryset = Post.objects.filter(status=1)
+    template_name = "blog/index.html"
+    paginate_by = 6
 
 
 def post_detail(request, slug):
-   """
-   Display an individual :model:`blog.Post`.
+    """
+    Display an individual :model:`blog.Post`.
 
 
-   **Context**
+    **Context**
 
 
-   ``post``
-       An instance of :model:`blog.Post`.
+    ``post``
+        An instance of :model:`blog.Post`.
 
 
-   **Template:**
+    **Template:**
 
 
-   :template:`blog/post_detail.html`
-   """
+    :template:`blog/post_detail.html`
+    """
+
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+    comments = post.comments.all().order_by("-created_on")
+    comment_count = post.comments.filter(approved=True).count()
+
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Comment submitted and awaiting approval'
+            )
+
+    comment_form = CommentForm()
+
+    return render(
+        request,
+        "blog/post_detail.html",
+        {
+            "post": post,
+            "comments": comments,
+            "comment_count": comment_count,
+            "comment_form": comment_form,
+        },
+    )
 
 
-   queryset = Post.objects.filter(status=1)
-   post = get_object_or_404(queryset, slug=slug)
-   comments = post.comments.all().order_by("-created_on")
-   comment_count = post.comments.filter(approved=True).count()
-  
-   if request.method == "POST":
-       comment_form = CommentForm(data=request.POST)
-       if comment_form.is_valid():
-           comment = comment_form.save(commit=False)
-           comment.author = request.user
-           comment.post = post
-           comment.save()
-           messages.add_message(
-               request, messages.SUCCESS,
-               'Comment submitted and awaiting approval'
-   )
-
-
-   comment_form = CommentForm()
-
-
-   return render(
-       request,
-       "blog/post_detail.html",
-       {
-           "post": post,
-           "comments": comments,
-           "comment_count": comment_count,
-           "comment_form": comment_form,
-       },
-   )
-
-
+# this requires user to be logged in
+@login_required
 def comment_edit(request, slug, comment_id):
-   """
-   view to edit comments
-   """
-   if request.method == "POST":
+    """
+    view to edit comments
+    """
+    if request.method == "POST":
+
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comment = get_object_or_404(Comment, pk=comment_id)
+        comment_form = CommentForm(data=request.POST, instance=comment)
+
+        if comment_form.is_valid() and comment.author == request.user:
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.approved = False
+            comment.save()
+            messages.add_message(request, messages.SUCCESS, 'Comment Updated!')
+        else:
+            messages.add_message(request, messages.ERROR,
+                                 'Error updating comment!')
+
+    return HttpResponseRedirect(reverse('post_detail', args=[slug]))
 
 
-       queryset = Post.objects.filter(status=1)
-       post = get_object_or_404(queryset, slug=slug)
-       comment = get_object_or_404(Comment, pk=comment_id)
-       comment_form = CommentForm(data=request.POST, instance=comment)
-
-
-       if comment_form.is_valid() and comment.author == request.user:
-           comment = comment_form.save(commit=False)
-           comment.post = post
-           comment.approved = False
-           comment.save()
-           messages.add_message(request, messages.SUCCESS, 'Comment Updated!')
-       else:
-           messages.add_message(request, messages.ERROR, 'Error updating comment!')
-
-
-   return HttpResponseRedirect(reverse('post_detail', args=[slug]))
-
-
-
-
+# this requires user to be logged in
+@login_required
 def comment_delete(request, slug, comment_id):
-   """
-   view to delete comment
-   """
-   queryset = Post.objects.filter(status=1)
-   post = get_object_or_404(queryset, slug=slug)
-   comment = get_object_or_404(Comment, pk=comment_id)
+    """
+    view to delete comment
+    """
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+    comment = get_object_or_404(Comment, pk=comment_id)
 
+    if comment.author == request.user:
+        comment.delete()
+        messages.add_message(request, messages.SUCCESS, 'Comment deleted!')
+    else:
+        messages.add_message(request, messages.ERROR,
+                             'You can only delete your own comments!')
 
-   if comment.author == request.user:
-       comment.delete()
-       messages.add_message(request, messages.SUCCESS, 'Comment deleted!')
-   else:
-       messages.add_message(request, messages.ERROR, 'You can only delete your own comments!')
-
-
-   return HttpResponseRedirect(reverse('post_detail', args=[slug]))
-
+    return HttpResponseRedirect(reverse('post_detail', args=[slug]))
